fc26b49d73b9f382014f778314365c0ed0ffffbf
Tymeckia Kendall <tykend@gmail.com>
2018-11-23 15:17:34 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,16 @@
+def instantiate_new_array 
+  my_new_array= Array.new
+   return my_new_array
+end
+
+def array_with_two_elements 
+  my_two_array = [ "a", "b"]
+end
+
+def first_element
+  taylor_swift=["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+    my_first_element = taylor_swift.first 
+end 
+  
+  
+  
