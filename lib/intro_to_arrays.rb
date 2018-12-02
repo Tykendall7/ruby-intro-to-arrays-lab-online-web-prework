4793416903584ece8fa48e57a81cd970ecff5f9b
Tymeckia Kendall <tykend@gmail.com>
2018-12-02 02:20:08 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,15 @@
+def instantiate_new_array 
+  my_new_array= Array.new
+   return my_new_array
+end
+
+def array_with_two_elements 
+  my_two_array = [ "a", "b"]
+end
+
+def first_element
+    taylor_swift.take[3]
+end 
+  
+  
+  
